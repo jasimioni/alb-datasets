94a923909424ab2058ebba693ce21171943f95c2
João André Simioni <jasimioni@gmail.com>
2025-03-06 01:43:13 -0300

    Update 102_bacen_ETL_v01_download_simples.R

--- bacen/scripts/102_bacen_ETL_v01_download_simples.R
+++ bacen/scripts/102_bacen_ETL_v01_download_simples.R
@@ -1,25 +1,27 @@
 # CLEAR ENVIRONMENT ############################################################
 
+# Remove all objects from the environment
+rm(list = ls())
+
+# Run garbage collection
+gc()
+
 # INSTALL AND LOAD PACKAGES ####################################################
 
-install_and_load_packages <- function(...) {
-  # Capture the arguments as a character vector
-  packages <- c(...)
-  
-  # Check if all inputs are strings
-  if (!all(sapply(packages, is.character))) {
-    stop("All input arguments must be strings representing package names.")
-  }
-  
-  for (pkg in packages) {
-    if (!require(pkg, character.only = TRUE)) {
-      install.packages(pkg, method = "wget")
-    } 
-    library(pkg, character.only = TRUE)
-  } 
+packages = c(
+  'GetBCBData',
+  'openxlsx',
+  'dplyr',
+  'tidyr'
+)
+# 
+# 
+ for (pkg in packages) {
+   if (!require(pkg, character.only = TRUE)) {
+     install.packages(pkg, repos = "https://cran.rstudio.com/", method = "libcurl")
+   } 
+   library(pkg, character.only = TRUE)
 }
-
-install_and_load_packages('GetBCBData', 'dplyr', 'tidyr', 'openxlsx')
 
 # TIME CONTROL #################################################################
 
